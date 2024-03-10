8250640543a8130df26c8e8079bda04673d787fb
Marc <marc.blattmann08@gmail.com>
2024-03-10 14:19:02 +0100

    Menu Hinzugefügt | Nur bei Fächer

---
+++ public/seiten/JS/Profile.js
@@ -0,0 +1,20 @@
+
+  document.getElementById("profile").addEventListener("click", function() {
+      var content = document.getElementById("profile-menu");
+     if (content.style.display === "none" || content.style.display === "") {
+          content.style.display = "flex"; // Sichtbar machen
+      } else {
+          content.style.display = "none"; // Unsichtbar machen
+      }
+  });
+
+  document.addEventListener("click", function(event) {
+      var content = document.getElementById("profile-menu");
+      var toggleButton = document.getElementById("profile");
+      var isClickInsideContent = content.contains(event.target);
+      var isClickInsideToggleButton = toggleButton.contains(event.target);
+
+      if (!isClickInsideContent && !isClickInsideToggleButton) {
+          content.style.display = "none"; // Unsichtbar machen, wenn außerhalb geklickt wird
+      }
+  });
